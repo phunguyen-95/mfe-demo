999f57e303768cb3320758e201e5ca72d2f4a17c
Phu Nguyen <phunguyen@Phus-MacBook-Pro.local>
2026-04-27 23:34:52 +0700

    [deploy]

--- shell/webpack.config.ts
+++ shell/webpack.config.ts
@@ -2,10 +2,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { ModuleFederationPlugin } = require("webpack").container;
 
 
-
-
 module.exports = {
-  mode: "development",
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   entry: "./src/index.ts",
   devServer: {
     port: 3000,
